9302597cdb265ca715f03c1fc9083756de5aafcf
Laura Horsley <horsleyshorses02@gmail.com>
2022-04-05 13:28:34 -0700

    Sum Demo

---
+++ sum-expenses.py
@@ -0,0 +1,12 @@
+expenses = [10.50, 8, 5, 15, 20, 5, 3]
+# sum = 0
+
+# for x in expenses:
+#     sum = sum + x
+
+# print("You spent $", sum, " on lunch this week.", sep='')
+
+# or
+total = sum(expenses)
+
+print("You spent $", total, " on lunch this week.", sep='')
