3a0aabb21eb54dd72a1e39fab58e29dc39857b10
Laura Horsley <horsleyshorses02@gmail.com>
2022-04-05 15:08:05 -0700

    Weather Updates

--- weather.py
+++ weather.py
@@ -1,21 +1,29 @@
 from urllib import request
 import requests
 
+def get_weather_desc_and_temp():
+    api_key = "71d5fccddfc8d606452bc623270e3f28"
+    city = "Orlando"
+    url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=imperial"
 
-api_key = "71d5fccddfc8d606452bc623270e3f28"
-city = "Orlando"
-url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=imperial"
+    request = requests.get(url)
+    json = request.json()
+    # print(json)
 
-request = requests.get(url)
-json = request.json()
-# print(json)
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
 
-description = json.get("weather")[0].get("description")
-print("Today's forecast is", description)
+    return {'description': description,
+            'temp_min': temp_min,
+            'temp_max': temp_max
+            }
 
-temp_min = json.get("main").get("temp_min")
-print("The minimum temperature is", temp_min)
+def main():
+    weather_dict = get_weather_desc_and_temp()
+    print("Today's forecast is", weather_dict.get('description'))
+    print("The minimum temperature is", weather_dict.get('temp_min'))
+    print("The maximum temperature is", weather_dict.get('temp_max'))
 
-temp_max = json.get("main").get("temp_max")
-print("The maximum temperature is", temp_max)
 
+main()
